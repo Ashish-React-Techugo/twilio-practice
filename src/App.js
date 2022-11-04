ba161adcf9f50ababe84422eb379babd658b7623
Ashish-Techugo <ashish.bisht@techugo.com>
2022-11-04 14:02:20 +0530

    basename again 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/twilio-practice">
+      <BrowserRouter>
       <div>
         <Link to='/screenrecording'>ScreenRecording</Link>
       </div>
